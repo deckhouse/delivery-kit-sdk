7dc934327db023dad238006315e16938ab81331c
Alexandr Zaytsev <alexandr.zaytsev@flant.com>
2025-07-03 20:34:25 +0300

    feat(signature): verify chain with optional root cert
    Signed-off-by: Alexandr Zaytsev <alexandr.zaytsev@flant.com>

--- pkg/signver/verifier.go
+++ pkg/signver/verifier.go
@@ -9,7 +9,7 @@ import (
 )
 
 func NewVerifierFromCert(_ context.Context, certRef string) (signature.Verifier, error) {
-	_, cert, err := LoadCertFromRef(certRef)
+	cert, err := LoadCertFromRef(certRef)
 	if err != nil {
 		return nil, fmt.Errorf("load cert from ref: %w", err)
 	}
