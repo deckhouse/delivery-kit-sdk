cba32eac37607ad9c16ff2067bfd3dda330d153b
Alexandr Zaytsev <alexandr.zaytsev@flant.com>
2025-10-22 14:49:26 +0300

    fix(signver): be consistent about namespace
    Signed-off-by: Alexandr Zaytsev <alexandr.zaytsev@flant.com>

--- pkg/signver/hashivault/auth_base.go
+++ pkg/signver/hashivault/auth_base.go
@@ -8,16 +8,16 @@ import (
 )
 
 type baseAuthenticator struct {
-	loginNamespace string
-	tokenID        string
-	tokenTTL       time.Duration
-	tokenIssuedAt  time.Time
+	authPath      string
+	tokenID       string
+	tokenTTL      time.Duration
+	tokenIssuedAt time.Time
 }
 
 func (b *baseAuthenticator) login(client *vault.Client, data map[string]interface{}) error {
 	b.tokenIssuedAt = time.Now()
 
-	resp, err := client.Logical().Write(fmt.Sprintf("/auth/%s/login", b.getLoginNamespace()), data)
+	resp, err := client.Logical().Write(fmt.Sprintf("/auth/%s/login", b.getAuthPath()), data)
 	if err != nil {
 		return fmt.Errorf("vault write: %w", err)
 	}
@@ -39,11 +39,11 @@ func (b *baseAuthenticator) login(client *vault.Client, data map[string]interfac
 	return nil
 }
 
-func (b *baseAuthenticator) getLoginNamespace() string {
-	if namespace := getVaultAuthPath(); namespace != "" {
-		return namespace
+func (b *baseAuthenticator) getAuthPath() string {
+	if authPath := getVaultAuthPath(); authPath != "" {
+		return authPath
 	}
-	return b.loginNamespace
+	return b.authPath
 }
 
 func (b *baseAuthenticator) Login(client *vault.Client) error {
